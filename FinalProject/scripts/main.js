5f5f60718963f256d577bf780b5f9dc147081da4
CJ-Griffin0826 <cg233293@umconnect.umt.edu>
2026-04-26 21:44:54 -0600

    Loop Moment

---
+++ FinalProject/scripts/main.js
@@ -0,0 +1,35 @@
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+
+variableName = Object.function(argument);
+
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "rgba(255, 200, 255, 0.8)";
+ctx.shadowColor = "rgba(255, 200, 255, 0.8)";
+ctx.shadowBlur = 20;
+
+let y = 0
+
+ctx.beginPath();
+ctx.arc(150, 150, 40, 0, Math.PI * 2);
+ctx.fill();
+
+function draw(){
+    ctx.clearRect(0, 0, canvas.clientWidth, canvas.height);
+
+    ctx.beginPlath();
+    ctx.arc(150, y, 40, 0, Math.PI * 2);
+    ctx.fill();
+
+    y += 1;
+
+    if (y > canvas.height) y = 0;
+}
+
+function loop(){
+    draw();
+    requestAnimationFrame(loop)
+}
+
+loop();
